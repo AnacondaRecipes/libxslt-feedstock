1b337fd8d9e5260fd27957e6f033a80d3e10351f
Nehal J Wani <nehaljw.kkd1@gmail.com>
2019-01-28 05:23:22 -0600

    Bump version to 1.1.33
    - Remove .la files before buildling
    - Fix xml2-config to avoid overlinking

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Avoid overlinking. zlib and liblzma are private to libxml2
+rm -f $PREFIX/lib/*.la
+sed -i.bak -e 's/-llzma //g' -e 's/-lz //g' $PREFIX/bin/xml2-config
+
 ./configure --prefix=$PREFIX \
             --with-libxml-prefix=$PREFIX
 
