cde4e5fccfab25d02b72b158d71a6b4f18c1efe2
Kai Tietz <ktietz@anaconda.com>
2021-08-18 10:12:17 +0200

    add osx-arm64 support, fix a dependency issue of patch on unix

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get an updated config.sub and config.guess
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
+
 # Avoid overlinking. zlib and liblzma are private to libxml2
 find "${PREFIX}/lib" -name "*.la" -delete -print
 sed -i.bak -e 's/-llzma //g' -e 's/-lz //g' $PREFIX/bin/xml2-config
