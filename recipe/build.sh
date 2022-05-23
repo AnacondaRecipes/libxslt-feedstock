3fa99d86522fe982f5648300323f5bf2b9d0df4c
Serhii Kupriienko <skupriienko@anaconda.com>
2022-05-23 12:54:06 +0000

    Update source url

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,8 @@ find "${PREFIX}/lib" -name "*.la" -delete -print
 sed -i.bak -e 's/-llzma //g' -e 's/-lz //g' $PREFIX/bin/xml2-config
 
 ./configure --prefix=$PREFIX \
-            --with-libxml-prefix=$PREFIX
+            --with-libxml-prefix=$PREFIX \
+            --without-python
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
