49efbe5e5716305e0c471f3c2cc93d44f337bcaf
Serhii Kupriienko <skupriienko@anaconda.com>
2022-05-25 11:16:35 +0000

    Add a flag with-icu

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,8 @@ sed -i.bak -e 's/-llzma //g' -e 's/-lz //g' $PREFIX/bin/xml2-config
 
 ./configure --prefix=$PREFIX \
             --with-libxml-prefix=$PREFIX \
-            --without-python
+            --without-python \ 
+            --with-icu
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
