301230cb60a7fc325c1307d5d7f522087916224b
Serhii Kupriienko <skupriienko@anaconda.com>
2022-05-25 11:25:07 +0000

    Add icu again

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 find "${PREFIX}/lib" -name "*.la" -delete -print
 sed -i.bak -e 's/-llzma //g' -e 's/-lz //g' $PREFIX/bin/xml2-config
 
-./configure --prefix=$PREFIX --with-libxml-prefix=$PREFIX --without-python --with-icu
+./configure --prefix=$PREFIX --with-libxml-prefix=$PREFIX --without-python
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
