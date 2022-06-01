5a3cfe2cee7f84256e6bf76d6c4c668e92aad9b7
anaconda-pkg-build <66446536+anaconda-pkg-build@users.noreply.github.com>
2022-06-01 04:29:01 -0500

    libxslt 1.1.35 (#1)
    * DOR update to 1.1.35
    
    * Update source url
    
    * Add icu in ignore_run_exports on osx
    
    * Add conda-build
    
    * Migration: OSXarm
    
    * Remove an old patch
    
    * Add lxml2 to LDFLAGS because of a missing symbol on osx-arm64
    
    * Use --without-python flag
    
    * Fix configure

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Get an updated config.sub and config.guess
-cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 # Avoid overlinking. zlib and liblzma are private to libxml2
 find "${PREFIX}/lib" -name "*.la" -delete -print
 sed -i.bak -e 's/-llzma //g' -e 's/-lz //g' $PREFIX/bin/xml2-config
 
-./configure --prefix=$PREFIX \
-            --with-libxml-prefix=$PREFIX
+# It looks like an missing symbol of libxml2 on osx-arm64
+if [[ ${target_platform} =~ .*arm64.* ]]; then
+    LDFLAGS="${LDFLAGS} -lxml2"
+fi
+
+./configure --prefix=$PREFIX --with-libxml-prefix=$PREFIX --without-python
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
